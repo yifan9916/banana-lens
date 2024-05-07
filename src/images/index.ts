54902828bec12008e1e8c739d1e7f6c3ca79c7bb
Yi Fan <yifan.9916@gmail.com>
2024-05-07 11:35:27 +0200

    rename files

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,7 +1,7 @@
 import { StaticImageData } from 'next/image';
 
 export { previewImages } from './preview/images';
-export { hiResImages } from './hires/images';
+export { hiResImages } from './hi-res/images';
 
 export type TODO_Image = {
   src: StaticImageData;
