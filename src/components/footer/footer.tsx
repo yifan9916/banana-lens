104a56590228fc1708937cf1f351f0280c889a4b
Yi Fan <yifan.9916@gmail.com>
2024-05-20 17:27:33 +0200

    add enough space to interact with switchers

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from '../theme-switcher';
 
 export const Footer = () => {
   return (
-    <footer className="flex justify-end items-end gap-2 p-12 pt-20">
+    <footer className="flex justify-end items-end gap-2 p-12 py-28">
       <ThemeSwitcher />
       <LocaleSwitcher />
     </footer>
