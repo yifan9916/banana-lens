cdfa34ca85c479c2e2d9b941639eb84ab87b06f0
Yi Fan <yifan.9916@gmail.com>
2024-05-30 16:28:09 +0200

    updates

--- src/app/[locale]/photography/page.tsx
+++ src/app/[locale]/photography/page.tsx
@@ -1,22 +1,21 @@
 import { useTranslations } from 'next-intl';
 
-import { LiveMessageThread } from '@/components/messages/live/live-message-thread';
 import {
-  MessageThread,
   type Message,
+  MessageThread,
 } from '@/components/messages/thread/message-thread';
 import { ChatWrapper } from '@/components/messages/chat-wrapper';
 
 export default function Page() {
   const dict = useTranslations('Photography.body');
 
-  const one: Message[] = [
+  const motivation: Message[] = [
     { body: dict('01') },
     { body: dict('02') },
     { body: dict('03') },
   ];
 
-  const two: Message[] = [
+  const gear: Message[] = [
     { body: dict('04') },
     { body: dict('05') },
     { body: dict('06') },
@@ -26,8 +25,8 @@ export default function Page() {
   return (
     <div id="description" className="px-10 max-w-2xl m-auto">
       <ChatWrapper>
-        <MessageThread messages={one} />
-        <LiveMessageThread messages={two} />
+        <MessageThread messages={motivation} />
+        <MessageThread messages={gear} />
       </ChatWrapper>
     </div>
   );
