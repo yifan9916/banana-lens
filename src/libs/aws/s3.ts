073eb9380f976d6285c35bd190c5083314d0370a
Yi Fan <yifan.9916@gmail.com>
2024-09-16 16:32:30 +0200

    hotfix

--- src/libs/aws/s3.ts
+++ src/libs/aws/s3.ts
@@ -5,21 +5,21 @@ import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
 // revisit when looking to implement file downloads
 // import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-import { env } from '@/env';
+// import { env } from '@/env';
 
 const MAX_FILE_SIZE_IN_MB = 1024 * 1024 * 50; // ~50MB
 
 const s3 = new S3Client({
-  region: env.AWS_BUCKET_REGION,
+  region: process.env.AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
 export const getPresignedPost = async (key: string, contentType: string) => {
   const { url, fields } = await createPresignedPost(s3, {
-    Bucket: env.AWS_BUCKET_NAME,
+    Bucket: process.env.AWS_BUCKET_NAME!,
     Key: key,
     Conditions: [
       ['content-length-range', 0, MAX_FILE_SIZE_IN_MB],
